de6455e05fae01555dfeaacdbdd5e284297508d4
ylsv <lisov1988@gmail.com>
2020-02-29 17:47:49 +0300

    add single news template, styles and final logic

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ function showNews() {
 function showMainNews(news) {
   let output = "";
   news.forEach(i => {
-    output += `<p class="main-news__item">${i.title}</p>`;
+    output += `<p class="main-news__item" id="${i.id}">${i.title}</p>`;
   });
   document.querySelector(".main-news").innerHTML = output;
 }
@@ -39,7 +39,7 @@ function showAllNews(news) {
   let output = "";
   news.forEach(i => {
     output += `
-      <article class="news-article">
+    <article class="news-article" id="${i.id}">
       <div class="news-article__time">${i.date.slice(11, 16)}</div>
       <div class="news-article__image">
         <img src="${i.image === undefined ? "no image" : i.image.link}" alt="${
@@ -58,6 +58,59 @@ function showAllNews(news) {
 
 
 
-function showArticle(){
+// show single news after click
+document.querySelector('.news-container').addEventListener('click', showSingleArticle);
 
+function showSingleArticle(e){
+  console.log(e.target);
+  getNews().then(data => {
+    showArticle(data.allNews);
+  });
+  
+  function showArticle(news) {
+    news.forEach(i => {
+      if(i.id === e.target.id || i.id === e.target.parentElement.parentElement.id) {
+        document.querySelector('.news-container').innerHTML = `
+        <div class="article">
+          <div class="article__backBtn">
+            <div class="backBtn">
+              <img src="/source/back.png" alt="back">
+              <div class="backBtn__text">назад</div>
+            </div>
+          </div>
+
+          <article class="article__content">
+            <div class="article__header">
+              <div class="article__time">${i.date.slice(0, 10).split('-').reverse().join('.')}, ${i.date.slice(11, 16)}</div>
+              <div class="article__header-wrapper">
+                <h3 class="article__header-text">${i.title}</h3>
+                <div class="article__header-short">${i.description}</div>
+              </div>
+            </div>
+            <div class="article__image">
+              <img src="${i.image === undefined ? "no image" : i.image.link}" alt="${
+                i.image === undefined ? "фото отсутствует" : i.title
+              }" />
+            </div>
+            <p class="article__text">${i.fulltext}</p>
+          </article>
+        </div>
+        `
+      }
+    })
+  };  
+}
+
+
+// handling back button on single news page
+document.querySelector('.news-container').addEventListener('click', goBack);
+
+function goBack(e){
+  if(e.target.classList.contains('article__backBtn')
+  || e.target.classList.contains('backBtn')
+  || e.target.classList.contains('backBtn__text')
+  || e.target.alt === 'back'
+  ) {
+    location.reload();
+  }
 }
